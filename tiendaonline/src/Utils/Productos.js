b77b1b9cfdbc4084e10137ec27fb973e0644e11d
JFEMENIA <jorgefemenia@gmail.com>
2023-11-15 20:42:59 -0300

    agregado utils/productos

---
+++ tiendaonline/src/Utils/Productos.js
@@ -0,0 +1,12 @@
+export const Productos = [
+  {
+    "id": "6767467467",
+    "title": "Hamburguesa con queso",
+    "img": new URL("https://cincosaltos.toque.com.ar/sistema/uploads/1600/articulos/683932327149.jpeg"),
+    "description": "Ingredientes Carne de ternera picada – 600 gramos Panes de hamburguesa – 4 unidades Queso cheddar – 4 lonchas Pimienta molida – al gusto Lechuga – 1 unidad",
+    "price": "$3500",
+    "sku": "824281241",
+    "stock": 200,
+  }  
+];
+  
--- tiendaonline/src/components/DetallesProducto.jsx
+++ tiendaonline/src/components/DetallesProducto.jsx
@@ -1,6 +1,6 @@
 import React,{useState} from 'react';
 
-export const ProductDetails = ({title,img,description,price,sku,stock}) => {
+export const DetallesProducto = ({title,img,description,price,sku,stock}) => {
   const [cantidad, setCantidad] = useState(1)
 
   const sumarProducto = () => {
@@ -11,15 +11,13 @@ export const ProductDetails = ({title,img,description,price,sku,stock}) => {
     setCantidad(cantidad-1)
   }
 
-  const compra = useToast();
+  const toastTrigger = document.getElementById('liveToastBtn')
+  const toastLiveExample = document.getElementById('liveToast')
 
-  function Comprar() {
-    compra({
-      title: 'Gracias por tu compra!',
-      description: "Te enviamos un email con los detalles de tu compra.",
-      status: 'success',
-      duration: 9000,
-      isClosable: true,
+  if (toastTrigger) {
+    const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
+    toastTrigger.addEventListener('click', () => {
+      toastBootstrap.show()
     })
   }
 
@@ -30,16 +28,34 @@ export const ProductDetails = ({title,img,description,price,sku,stock}) => {
         <div class="card-body">
           <h5 class="card-title">{title}</h5>
           <p class="card-text">Descripción: {description}</p>
-          <p class="card-text"><small class="text-body-secondary">SKU: SKU: {sku}</small></p>
+          <p class="card-text"><small class="text-body-secondary">SKU: {sku}</small></p>
           <p class="card-text">Precio: {price}</p>
           <p class="card-text"><small class="text-body-secondary">Cantidad disponible: {stock}</small></p>
-
+          
           <p class="card-text">Cantidad a comprar: {cantidad}</p>
-          <button type="button" class="btn btn-danger" onClick={restarProducto} isDisabled={cantidad==1?true:false}>Quitar</button>
-          <button type="button" class="btn btn-success" onClick={sumarProducto} isDisabled={cantidad>=stock?true:false}>Agregar</button>          
+          <div className="card-body d-flex justify-content-center gap-2">
+            <button type="button" class="btn btn-danger" onClick={restarProducto} isDisabled={cantidad==1?true:false}>Quitar</button>
+            <button type="button" class="btn btn-success" onClick={sumarProducto} isDisabled={cantidad>=stock?true:false}>Agregar</button>          
+          </div>         
+          <div className="card-body d-flex justify-content-center gap-2">
+            <button type="button" class="btn btn-primary" id="liveToastBtn">Comprar</button>
 
-          <a href="#" class="btn btn-primary" onClick={Comprar}>Comprar</a>
-          <a href="#" class="btn btn-primary">Agregar al carrito</a>
+            <div class="toast-container position-fixed bottom-0 end-0 p-3">
+              <div id="liveToast" class="toast" role="alert" aria-live="assertive" aria-atomic="true">
+                <div class="toast-header">
+                  {/* <img src="..." class="rounded me-2" alt="..."/> */}
+                  <strong class="me-auto">Compra registrada</strong>
+                  <small>Tienda Grupo 5</small>
+                  <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+                </div>
+                <div class="toast-body">
+                  Gracias por tu compra!
+                </div>
+              </div>
+            </div>
+
+            <a href="#" class="btn btn-primary">Agregar al carrito</a>
+          </div>
         </div>
     </div>
     </>
