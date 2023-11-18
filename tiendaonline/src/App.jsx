75f41066fcdc3ca5cb786ebffa7036034f752e2b
Damian Ottone <106179924+damianottone@users.noreply.github.com>
2023-11-18 00:49:13 -0300

    ruta contacto

--- tiendaonline/src/App.jsx
+++ tiendaonline/src/App.jsx
@@ -4,6 +4,7 @@ import { Landing } from './Components/Landing.jsx';
 import { DetallesProducto } from './Components/DetallesProducto.jsx';
 import { Carrito } from './Components/Carrito.jsx';
 import { ResetCarrito } from './Components/ResetCarrito.jsx';
+import { Contacto } from `./Components/Contacto.jsx`
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/detalles/:id" element={<DetallesProducto />} />
         <Route path="/carrito" element={<Carrito />} />
         <Route path="/carrito/reset" element={<ResetCarrito />} />
+        <Route path="/contacto" element={<Contacto />} />
       </Routes>
     </Router>
   );
