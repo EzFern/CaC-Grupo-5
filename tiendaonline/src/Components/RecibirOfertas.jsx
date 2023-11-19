7b0ea616754a60310d5eb5bb7e465e9a0917c641
Pablo Costa <46192942+pcosta0@users.noreply.github.com>
2023-11-18 22:52:56 -0300

    crear componente RecibirOfertas

--- tiendaonline/src/Components/RecibirOfertas.jsx
+++ tiendaonline/src/Components/RecibirOfertas.jsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import React from "react";
+import "./RecibirOfertas.css";
 
 export const RecibirOfertas = () => {
   return (
-    <div>RecibirOfertas</div>
-  )
-}
+    <div>
+      <div class="recibir_ofertas d-flex align-items-center justify-content-center">
+        <div className="col-10 d-flex justify-content-center">
+          <div className="">
+            <h1>¡Recibi nuestras ofertas!</h1>
+            {/* <p className="text-center">formulario.</p> */}
+            <div className="d-flex  justify-content-center">
+              <input className="col-12 mx-1" type="email" name="" id="" placeholder="Email" />
+              <input className="col-2" type="submit" value=">" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
