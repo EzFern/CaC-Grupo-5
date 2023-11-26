44783b632cddaef7e7ebc46466c11e36709edb84
fabigera88 <fabianzandri@gmail.com>
2023-11-26 18:28:23 -0300

    Header

--- tiendaonline/src/Components/EncabezadoDePagina.jsx
+++ tiendaonline/src/Components/EncabezadoDePagina.jsx
@@ -1,7 +1,57 @@
-import React from 'react'
+import React from "react";
+import "./EncabezadoDePagina.css";
 
 export const EncabezadoDePagina = () => {
   return (
-    <div>EncabezadoDePagina</div>
-  )
-}
+    <>
+      <header>
+        <nav className="navbar">
+          <div className="buscar_antojo d-flex align-items-left justify-content-left">
+            <div className="col-11 d-flex justify-content-left">
+              <div className="d-flex justify-content-left">
+                <input className="my-0 mt-2 me-1 px-4"
+                  type="Search"
+                  name="Buscador"
+                  placeholder="Buscar"
+                />
+                <button className="btn btn-secondary mt-2 px-3 py-2">
+                  <img src="/src/assets/iconos/Lupa.png" width="30" />
+                </button>
+              </div>
+            </div>
+          </div>
+
+          <div className="nav-logo-container">
+            <img className="nav-logo-img" 
+              src="/src/assets/iconos/Logo.png" alt="Fast Food 5"/>
+          </div>
+
+          <ul className="nav nav-underline justify-content-end">
+            <li className="nav-item dropdown">
+              <a className="nav-link dropdown-toggle"
+                data-bs-toggle="dropdown"
+                href="#"
+                role="button"
+                aria-expanded="false"
+              >
+                <img className="img-nav-list" src="/src/assets/iconos/Perfil.png"/>{" "}
+                MI CUENTA
+              </a>
+              <ul className="dropdown-menu">
+                <li><a className="dropdown-item" href="#">Iniciar Sesion</a></li>
+                <li><hr className="dropdown-divider"></hr></li>
+                <li><a className="dropdown-item" href="#">Registrarme</a></li>
+              </ul>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                <img className="img-nav-list" src="/src/assets/iconos/Carrito.png"/>
+                CARRITO
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
+};
