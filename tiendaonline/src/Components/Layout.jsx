dfa8f5ef15abc1a43ece4f30ad6b210f3bf3515f
JFEMENIA <jorgefemenia@gmail.com>
2023-11-27 13:52:15 -0300

    agregado menu a layout

--- tiendaonline/src/Components/Layout.jsx
+++ tiendaonline/src/Components/Layout.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import { EncabezadoDePagina } from "./EncabezadoDePagina";
+import { Menu } from "./Menu"
 import { PieDePagina } from "./PieDePagina";
 
 export const Layout = () => {
   return (
     <div className="container">
       <EncabezadoDePagina />
+      <Menu />
       <Outlet />
       <PieDePagina />
     </div>
