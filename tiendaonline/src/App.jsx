1b5d47211f4fb805a49d1a42342355a73035512c
Pablo Costa <46192942+pcosta0@users.noreply.github.com>
2023-11-27 12:13:06 -0300

    actualizar App con Layout

--- tiendaonline/src/App.jsx
+++ tiendaonline/src/App.jsx
@@ -1,26 +1,31 @@
-import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Landing } from './Components/Landing.jsx';
-import { DetallesProducto } from './Components/DetallesProducto.jsx';
-import { Carrito } from './Components/Carrito.jsx';
-import { ResetCarrito } from './Components/ResetCarrito.jsx';
-import { Contacto } from './Components/Contacto.jsx';
-import {ComoComprar} from './Components/ComoComprar.jsx';
-import { PreguntasFrecuentes } from './Components/PreguntasFrecuentes.jsx';
-import { BasesYCondiciones } from './Components/BasesYCondiciones.jsx'
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Landing } from "./Components/Landing.jsx";
+import { DetallesProducto } from "./Components/DetallesProducto.jsx";
+import { Carrito } from "./Components/Carrito.jsx";
+import { ResetCarrito } from "./Components/ResetCarrito.jsx";
+import { Contacto } from "./Components/Contacto.jsx";
+import { ComoComprar } from "./Components/ComoComprar.jsx";
+import { PreguntasFrecuentes } from "./Components/PreguntasFrecuentes.jsx";
+import { BasesYCondiciones } from "./Components/BasesYCondiciones.jsx";
+import { AcercaDe } from "./Components/AcercaDe.jsx";
+import { Layout } from "./Components/Layout.jsx";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/detalles/:id" element={<DetallesProducto />} />
-        <Route path="/carrito" element={<Carrito />} />
-        <Route path="/carrito/reset" element={<ResetCarrito />} />
-        <Route path="/contacto" element={<Contacto />} />
-        <Route path="/como-comprar" element={<ComoComprar />} />
-        <Route path="/preguntas-frecuentes" element={<PreguntasFrecuentes />} />
-        <Route path="/bases-y-condiciones" element={<BasesYCondiciones />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Landing />} />
+          <Route path="/detalles/:id" element={<DetallesProducto />} />
+          <Route path="/carrito" element={<Carrito />} />
+          <Route path="/carrito/reset" element={<ResetCarrito />} />
+          <Route path="/contacto" element={<Contacto />} />
+          <Route path="/como-comprar" element={<ComoComprar />} />
+          <Route path="/preguntas-frecuentes" element={<PreguntasFrecuentes />}/>
+          <Route path="/acerca-de" element={<AcercaDe />} />
+          <Route path="/bases-y-condiciones" element={<BasesYCondiciones />} />
+        </Route>
       </Routes>
     </Router>
   );
