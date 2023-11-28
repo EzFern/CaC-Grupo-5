07459e914ff20115d0273a3b22f265b3494bb6b4
EzFern <alex1s22@hotmail.com>
2023-11-27 21:02:23 -0300

    Footer hecho

--- tiendaonline/src/Components/PieDePagina.jsx
+++ tiendaonline/src/Components/PieDePagina.jsx
@@ -1,7 +1,68 @@
-import React from 'react'
+import React from 'react';
+import "./PieDePagina.css";
 
 export const PieDePagina = () => {
   return (
-    <div>PieDePagina</div>
+    <div className="d-flex justify-content-center db-danger">
+
+      <footer className="piedepagina container text-center ">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-3 col-sm-6">
+              <div className="widget_text">
+                <p className="widget-title"><span>Contacto</span></p>
+                <div className="textwidget">
+                  <p>Av. Libertador 1562 - CABA <br></br> <a href="tel:549 01230456">Tel. (549) 15 01230456</a> <br></br> <a href="mailto:mail@fastfoodfive.com.ar">mail@fastfoodfive.com.ar</a>  </p>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-md-3 col-sm-6">
+              <div className="widget_text">
+                <p className="widget-title"><span>Formas de Pago</span></p>
+                <div className="textwidget">
+                <p>Efectivo o transferencia bancaria.</p>
+                <p>COMO COMPRAR</p>
+                <p>Preguntas frecuentes</p>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-md-3 col-sm-6">
+              <div className="widget_text">
+                <p className="widget-title"><span>ENVIOS</span></p>
+                <div className="textwidget"><p>Ventas: Lunes a viernes - 8 a 20hs.</p>
+                  <p>Entrega de pedidos: Lunes a viernes - 9 a 23hs.</p>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-md-3 col-sm-6">
+              <div className="widget_text widget_custom_html">
+                  <p>© Copyright Fast Food 5. </p><br></br><p> Todos los derechos reservados.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </div>
   )
 }
+
+{/* //  <div className="d-flex justify-content-center db-danger">
+    //     <div className="piedepagina container text-center">
+    //         <div className="row">
+    //           <div className='col'>
+    //             <p>COMO COMPRAR</p>
+    //             <p>Preguntas frecuentes</p>
+    //             <p>Bases y condiciones</p>
+    //           </div>
+    //           <div className='col'>
+    //             <p>CONTACTO</p>
+    //               <p> 549 01230456   </p>
+    //             <p>mail@fastfoodfive.com.ar</p>
+    //             <p>Av. Libertador 1562 - CABA </p>
+    //           </div>
+    //         </div>
+    //     </div>
+    //  </div> */}
