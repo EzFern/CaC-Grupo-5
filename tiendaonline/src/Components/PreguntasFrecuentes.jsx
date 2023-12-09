9e900e4fa83ee90102f3d2195f29b73f1aa2a260
Pablo Costa <46192942+pcosta0@users.noreply.github.com>
2023-12-09 19:47:32 -0300

    agregar contenido a PreguntasFrecuentes

--- tiendaonline/src/Components/PreguntasFrecuentes.jsx
+++ tiendaonline/src/Components/PreguntasFrecuentes.jsx
@@ -1,7 +1,133 @@
-import React from 'react'
+import React from "react";
 
 export const PreguntasFrecuentes = () => {
   return (
-    <div>Preguntas Frecuentes</div>
-  )
-}
+    <>
+      <div className="container-fluid">
+        <div className="row cards_categorias mx-0 my-3 text-left">
+          <h2>Preguntas frecuentes</h2>
+
+          <p>
+            <strong>1. ¿Qué tipo de productos gourmet ofrecen?</strong>
+          </p>
+          <p>
+            Nuestra selección gastronómica se centra en la excelencia de dos
+            clásicos reinventados para el paladar sofisticado: pizzas de masa
+            madre con ingredientes de origen controlado y hamburguesas donde la
+            carne premium se encuentra con panes artesanales y complementos de
+            cultivo local. Descubra sabores tradicionales reinventados para el
+            paladar moderno.
+          </p>
+
+          <p>
+            <strong>
+              2. ¿Cómo garantizan la frescura y calidad de sus productos?
+            </strong>
+          </p>
+          <p>
+            Cada pizza y hamburguesa es preparada bajo pedido y con técnica sin
+            par, asegurando que cada ingrediente muestre su máximo potencial.
+            Además, nuestros envases termorregulados preservan el punto perfecto
+            del horneado y el equilibrio de sabores hasta su mesa.
+          </p>
+
+          <p>
+            <strong>
+              3. ¿Realizan envíos a nivel nacional o internacional?
+            </strong>
+          </p>
+          <p>
+            Nos enorgullecemos de conectar destinos con sabores, llevando
+            nuestras creaciones gourmet a nivel nacional. Cuidamos que cada
+            entrega mantenga la integridad de una experiencia culinaria recién
+            preparada, aunque por ahora limitamos nuestro servicio a fronteras
+            nacionales.
+          </p>
+
+          <p>
+            <strong>4. ¿Qué opciones de pago aceptan?</strong>
+          </p>
+          <p>
+            Aceptamos todas las principales tarjetas de crédito y débito, así
+            como pagos electrónicos a través de plataformas seguras. Nuestra
+            pasarela de pagos está blindada con las últimas tecnologías para
+            garantizar transacciones sin contratiempos y con total confianza.
+          </p>
+
+          <p>
+            <strong>5. ¿Cómo puedo hacer un seguimiento de mi pedido?</strong>
+          </p>
+          <p>
+            Al confirmar su pedido, le proporcionaremos un enlace con un número
+            de seguimiento único. De esta manera, podrá verificar en tiempo real
+            el progreso de su pizza o hamburguesa gourmet desde nuestra cocina
+            hasta su dirección.
+          </p>
+
+          <p>
+            <strong>
+              6. ¿Ofrecen opciones de regalos o envolturas especiales para
+              ocasiones?
+            </strong>
+          </p>
+          <p>
+            Ofrecemos un servicio exclusivo para presentar su regalo culinario.
+            Elija entre una variedad de empaques de diseño y añada un mensaje
+            personalizado que entregaremos en una tarjeta distinguida junto con
+            su pedido.
+          </p>
+
+          <p>
+            <strong>
+              7. ¿Cómo manejan las devoluciones o problemas con un pedido?
+            </strong>
+          </p>
+          <p>
+            Si su experiencia no refleja los altos estándares de nuestras pizzas
+            y hamburguesas, le invitamos a contactarnos de inmediato.
+            Evaluaremos su situación con prioridad y ofreceremos la solución
+            apropiada, ya sea un reemplazo prioritario o un reembolso completo.
+          </p>
+
+          <p>
+            <strong>
+              8. ¿Tienen promociones, ofertas especiales o programa de
+              fidelidad?
+            </strong>
+          </p>
+          <p>
+            Los clientes distinguidos merecen beneficios exclusivos. Nos
+            complace invitarlos a unirse a nuestro programa de fidelización,
+            donde acumularán puntos con cada compra, accederán a promociones
+            secretas y disfrutarán de descuentos especiales.
+          </p>
+
+          <p>
+            <strong>
+              9. ¿Cómo puedo garantizar que mi información personal y bancaria
+              está segura al comprar?
+            </strong>
+          </p>
+          <p>
+            La seguridad de nuestros clientes es primordial. Nuestra plataforma
+            utiliza sistemas de cifrado avanzados y cumplimos con todas las
+            regulaciones de seguridad de datos para garantizar que su
+            información personal y financiera está protegida en cada paso del
+            proceso de compra.
+          </p>
+
+          <p>
+            <strong>10. ¿Puedo personalizar mi pedido?</strong>
+          </p>
+          <p>
+            Sabemos que cada paladar es único, y es por ello que ofrecemos la
+            opción de personalizar su pizza o hamburguesa. Elija entre una serie
+            de ingredientes gourmet seleccionados para crear una obra maestra
+            culinaria que se ajuste a sus preferencias o necesidades dietéticas
+            específicas.
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
