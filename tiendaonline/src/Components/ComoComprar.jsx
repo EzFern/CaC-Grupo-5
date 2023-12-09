c1b7e379a6eef8d939e360c91d5c5d76481c6097
Pablo Costa <46192942+pcosta0@users.noreply.github.com>
2023-12-09 19:45:44 -0300

    agregar contenido a ComoComprar

--- tiendaonline/src/Components/ComoComprar.jsx
+++ tiendaonline/src/Components/ComoComprar.jsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import React from "react";
 
 export const ComoComprar = () => {
   return (
-    <div>Como Comprar</div>
-  )
-}
+    <>
+      <div className="container-fluid">
+        <div className="row cards_categorias mx-0 my-3 text-left">
+          <h2>¿Como comprar?</h2>
+          <p>
+            Realizar una compra en nuestra tienda es muy fácil. Comience
+            explorando la página principal para seleccionar los productos de su
+            interés. A continuación, especifique la cantidad deseada y diríjase
+            al carrito de compras para revisar y confirmar su pedido. Inicie
+            sesión en su cuenta y finalice la compra eligiendo su método de pago
+            preferido.
+          </p>
+          <p>
+            Si desea obtener mas información sobre productos o diferentes formas
+            de pago no dude en ponerse en contacto.
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
